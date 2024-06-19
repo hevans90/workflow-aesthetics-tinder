b3d24d2b117fb75bb0f6b466e8e113609c8245fc
Harri Evans <hevans9000@gmail.com>
2024-06-19 13:29:23 -0300

    fix(layout): mobile resolution overlaps of logout/logo

--- src/App.tsx
+++ src/App.tsx
@@ -41,45 +41,43 @@ function App() {
   };
 
   return (
-    <>
-      <div
-        className="flex h-full w-screen flex-col items-center gap-12 bg-primary-50 px-4 py-24"
-        style={{
-          backgroundImage: 'url(/noise.svg),url(/dotgrid.svg)',
-          backgroundPositionX: '50%, 50%',
-          backgroundPositionY: '0px, 0px',
-          backgroundSize: 'auto, auto',
-        }}
-      >
-        <>
-          <img className="absolute left-10 top-10 w-36" src={workFlowSvg} />
-          <section className="flex flex-col items-center gap-8">
-            <h1 className="text-center font-crimson text-5xl">
-              Aesthetics Tinder
-            </h1>
-            <h2 className="text-center text-xl font-light text-secondary-950 text-opacity-80 opacity-80">
-              Find the perfect match to your pedantic design needs!
-            </h2>
-          </section>
-          {apiKey ? (
-            <>
-              <section className="flex w-fit flex-col items-center gap-8">
-                <DebouncedSearchInput
-                  searching={searching}
-                  onSearch={(q) => handleSearch(q)}
-                />
+    <div
+      className="flex h-full w-screen flex-col items-center gap-12 bg-primary-50 px-4 py-24"
+      style={{
+        backgroundImage: 'url(/noise.svg),url(/dotgrid.svg)',
+        backgroundPositionX: '50%, 50%',
+        backgroundPositionY: '0px, 0px',
+        backgroundSize: 'auto, auto',
+      }}
+    >
+      <>
+        <img className="absolute left-6 top-6 w-36" src={workFlowSvg} />
+        <section className="flex flex-col items-center gap-8">
+          <h1 className="text-center font-crimson text-5xl">
+            Aesthetics Tinder
+          </h1>
+          <h2 className="text-center text-xl font-light text-secondary-950 text-opacity-80 opacity-80">
+            Find the perfect match to your pedantic design needs!
+          </h2>
+        </section>
+        {apiKey ? (
+          <>
+            <section className="flex w-fit flex-col items-center gap-8">
+              <DebouncedSearchInput
+                searching={searching}
+                onSearch={(q) => handleSearch(q)}
+              />
 
-                {searching ? 'Gathering new prospects...' : <Swiper />}
-              </section>
+              {searching ? 'Gathering new prospects...' : <Swiper />}
+            </section>
 
-              <Logout className="absolute right-10 top-8 px-12 font-crimson text-2xl" />
-            </>
-          ) : (
-            <Login />
-          )}
-        </>
-      </div>
-    </>
+            <Logout className="absolute right-6 top-4 px-12 font-crimson text-2xl" />
+          </>
+        ) : (
+          <Login />
+        )}
+      </>
+    </div>
   );
 }
 
